9dc6a0900b8d190444de14a510e40c627e5df588
Nikhil Prasad <contact.nikhilprasad@gmail.com>
2026-04-28 11:18:40 +0530

    test(db): populate relational tables with real-world dummy data for API testing

---
+++ database/seed.sql
@@ -0,0 +1,175 @@
+--Iserting dummy data for master.users TABLE
+
+INSERT INTO master.users (username, email, password_hash, is_active) VALUES 
+('sagar_back', 'sagar@vizosyn.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('ayush_front', 'ayush@vizosyn.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('sania_data', 'sania@vizosyn.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('samriddhi_ui', 'samriddhi@vizosyn.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('rahul_ops', 'rahul@vizosyn.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('neha_cloud', 'neha@vizosyn.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('karan_sec', 'karan@vizosyn.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('rohit_qa', 'rohit@vizosyn.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('amit_fullstack', 'amit@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('priya_ui', 'priya@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('vikram_data', 'vikram@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('sneha_devops', 'sneha@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('rohan_backend', 'rohan@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('pooja_react', 'pooja@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('aditya_cloud', 'aditya@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('kavita_sec', 'kavita@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('manish_qa', 'manish@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('riya_ml', 'riya@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('kunal_python', 'kunal@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('swati_design', 'swati@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('tarun_aws', 'tarun@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('megha_node', 'megha@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('yash_graphql', 'yash@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('anjali_vue', 'anjali@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('kabir_go', 'kabir@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('nidhi_docker', 'nidhi@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('prateek_sql', 'prateek@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('shruti_agile', 'shruti@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('varun_dev', 'varun@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('ritu_test', 'ritu@example.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('ghost_akash', 'akash.ghost@test.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('ghost_vivek', 'vivek.ghost@test.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('ghost_simran', 'simran.ghost@test.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true),
+('ghost_rajat', 'rajat.ghost@test.com', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjIQ68Y.Oq', true);
+
+
+--Iserting dummy data for master.locations TABLE
+
+INSERT INTO master.locations (city, state) VALUES 
+('New Delhi', 'Delhi'),
+('Bhilai', 'Chhattisgarh'),
+('Bangalore', 'Karnataka'),
+('Pune', 'Maharashtra'),
+('Hyderabad', 'Telangana'),
+('Gurgaon', 'Haryana'),
+('Noida', 'Uttar Pradesh');
+
+
+--Iserting dummy data for master.profiles TABLE
+
+INSERT INTO master.profiles (user_id, location_id, full_name, bio, github_url, linkedin_url) VALUES 
+(1, 2, 'Sagar', 'Backend Architect | PostgreSQL', 'github.com/sagar', 'linkedin.com/in/sagar'),
+(2, 3, 'Ayush', 'Frontend Specialist | React', 'github.com/ayush', 'linkedin.com/in/ayush'),
+(3, 4, 'Sania', 'Data Scientist | ML', 'github.com/sania', 'linkedin.com/in/sania'),
+(4, 4, 'Samriddhi', 'UI/UX Designer | Figma', 'github.com/sam', 'linkedin.com/in/sam'),
+(5, 5, 'Rahul', 'DevOps Engineer', 'github.com/rahul', 'linkedin.com/in/rahul'),
+(6, 6, 'Neha', 'Cloud Architect | AWS', 'github.com/neha', 'linkedin.com/in/neha'),
+(7, 1, 'Karan', 'Security Operations', 'github.com/karan', 'linkedin.com/in/karan'),
+(8, 2, 'Rohit', 'QA Automation', 'github.com/rohit', 'linkedin.com/in/rohit'),
+(9, 3, 'Amit Sharma', 'Full-Stack Developer', 'github.com/amit', 'linkedin.com/in/amit'),
+(10, 4, 'Priya Singh', 'Frontend Engineer', 'github.com/priya', 'linkedin.com/in/priya'),
+(11, 5, 'Vikram Gupta', 'Backend Dev', 'github.com/vikram', 'linkedin.com/in/vikram'),
+(12, 6, 'Sneha Patel', 'DevOps Engineer', 'github.com/sneha', 'linkedin.com/in/sneha'),
+(13, 7, 'Rohan Verma', 'Backend Ninja', 'github.com/rohan', 'linkedin.com/in/rohan'),
+(14, 1, 'Pooja Reddy', 'React Developer', 'github.com/pooja', 'linkedin.com/in/pooja'),
+(15, 2, 'Aditya Rao', 'Cloud Architect', 'github.com/aditya', 'linkedin.com/in/aditya'),
+(16, 3, 'Kavita Das', 'Security Specialist', 'github.com/kavita', 'linkedin.com/in/kavita'),
+(17, 4, 'Manish Tiwari', 'QA Engineer', 'github.com/manish', 'linkedin.com/in/manish'),
+(18, 5, 'Riya Kapoor', 'Machine Learning', 'github.com/riya', 'linkedin.com/in/riya'),
+(19, 6, 'Kunal Joshi', 'Python Scripter', 'github.com/kunal', 'linkedin.com/in/kunal'),
+(20, 7, 'Swati Menon', 'Product Designer', 'github.com/swati', 'linkedin.com/in/swati'),
+(21, 1, 'Tarun Bhatia', 'Solutions Architect', 'github.com/tarun', 'linkedin.com/in/tarun'),
+(22, 2, 'Megha Nair', 'Node.js Developer', 'github.com/megha', 'linkedin.com/in/megha'),
+(23, 3, 'Yash Agarwal', 'API Designer', 'github.com/yash', 'linkedin.com/in/yash'),
+(24, 4, 'Anjali Roy', 'Vue.js Fanatic', 'github.com/anjali', 'linkedin.com/in/anjali'),
+(25, 5, 'Kabir Malhotra', 'Golang Developer', 'github.com/kabir', 'linkedin.com/in/kabir'),
+(26, 6, 'Nidhi Desai', 'Kubernetes Operator', 'github.com/nidhi', 'linkedin.com/in/nidhi'),
+(27, 7, 'Prateek Jain', 'Database Admin', 'github.com/prateek', 'linkedin.com/in/prateek'),
+(28, 1, 'Shruti Bansal', 'Scrum Master', 'github.com/shruti', 'linkedin.com/in/shruti'),
+(29, 2, 'Varun Chawla', 'SDE', 'github.com/varun', 'linkedin.com/in/varun'),
+(30, 3, 'Ritu Pandey', 'Test Engineer', 'github.com/ritu', 'linkedin.com/in/ritu');
+
+
+--Iserting dummy data for master.skills TABLE
+
+INSERT INTO master.skills (skill_name) VALUES 
+('Python'), 
+('FastAPI'), 
+('PostgreSQL'), 
+('Next.js'), 
+('React'), 
+('Tailwind CSS'), 
+('Docker'), 
+('AWS'), 
+('Machine Learning'), 
+('Figma'),
+('TypeScript'), 
+('Node.js'), 
+('MongoDB'), 
+('GraphQL'), 
+('Kubernetes');
+
+
+--Iserting dummy data for master.skill_users TABLE
+
+INSERT INTO master.user_skills (user_id, skill_id) VALUES 
+(1, 1), (1, 2), (1, 3), (1, 7),
+(2, 4), (2, 5), (2, 6), (2, 11), 
+(3, 1), (3, 9), (3, 3), 
+(4, 10), (4, 6), 
+(5, 7), (5, 8), (5, 15), 
+(6, 8), (6, 15),
+(7, 1), (7, 7), 
+(8, 1), (8, 4),
+(9, 5), (10, 12), (11, 13), (12, 14), (13, 1),
+(14, 2), (15, 3), (16, 4), (17, 5), (18, 6),
+(19, 7), (20, 8), (21, 9), (22, 10), (23, 11),
+(24, 12), (25, 13), (26, 14), (27, 15), (28, 1),
+(29, 2), (30, 3);
+
+
+--Iserting dummy data for collaboration.teams TABLE
+
+INSERT INTO collaboration.teams (name, description, admin_id, max_members, status) VALUES 
+('Backend Ninjas', 'Scaling PostgreSQL and FastAPI APIs.', 1, 5, 'Open'),
+('React Rangers', 'Frontend performance and UX execution.', 2, 3, 'Full'),
+('Data Demons', 'ML models and data pipelines.', 3, 4, 'Open'),
+('Cloud Cartel', 'AWS Infrastructure and Kubernetes clusters.', 6, 2, 'Full'),
+('Design Divas', 'Figma prototypes and wireframes.', 4, 5, 'Open'),
+('SecOps Squad', 'Security audits and penetration testing.', 7, 3, 'Open'),
+('QA Mavericks', 'Automated testing and bug hunting.', 8, 4, 'Open'),
+('Alpha Tech', 'Incubation team for experimental features.', 9, 5, 'Open'),
+('Omega Stack', 'Legacy code migration team.', 10, 5, 'Open'),
+('DevOps Dragoons', 'Deployment and pipelines.', 5, 5, 'Open');
+
+
+--Iserting dummy data for collaboration.team_members TABLE
+
+INSERT INTO collaboration.team_members (team_id, user_id, is_active) VALUES 
+
+(1, 1, true),
+(1, 11, true), 
+(1, 12, true),
+(2, 2, true), 
+(2, 13, true), 
+(2, 14, true),
+(3, 3, true),
+(3, 15, true),
+(4, 6, true),
+(4, 16, true),
+(5, 4, true), 
+(5, 17, true),
+(6, 7, true), 
+(6, 18, true),
+(7, 8, true), 
+(7, 19, true),
+(8, 9, true), 
+(8, 20, true),
+(10, 5, true), 
+(10, 22, true);
+
+
+--Iserting dummy data for collaboration.teams_requests TABLE
+
+INSERT INTO collaboration.team_requests (team_id, user_id, status, message) VALUES 
+(1, 23, 'Pending', 'Sir, I can handle API optimization.'),
+(5, 24, 'Pending', 'Need design review from UI/UX team.'),
+(3, 25, 'Pending', 'I know Pandas and ML, let me in.'),
+(2, 13, 'Accepted', 'Welcome to the React team!'),
+(10, 22, 'Accepted', 'DevOps guy is here.'),
+(2, 26, 'Rejected', 'Sorry, team is already Full.'),
+(4, 27, 'Rejected', 'We only need AWS experts right now.');
